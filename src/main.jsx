d56d75581a531b701e12c94de98d36e2b8d7d7b5
Samba <basamba1990@yahoo.fr>
2025-09-15 02:00:20 +0000

    Fix import case for directory.jsx in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import AuthCallback from './pages/AuthCallback.jsx';
-import ResetPassword from './pages/ResetPassword.jsx';
-import RecordVideo from './pages/RecordVideo.jsx';
-import VideoSuccess from './pages/VideoSuccess.jsx';
-import Directory from './pages/Directory.jsx';
-import UserRegistration from './components/UserRegistration.jsx';
+import AuthCallback from '@/pages/AuthCallback.jsx';
+import ResetPassword from '@/pages/ResetPassword.jsx';
+import RecordVideo from '@/pages/record-video.jsx';
+import VideoSuccess from '@/pages/video-success.jsx';
+import Directory from '@/pages/directory.jsx';
+import UserRegistration from '@components/UserRegistration.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
